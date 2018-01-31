7f2324947be9c7a13134e72574911f3bd839937e
proson <proson@163.com>
2018-01-31 13:44:44 +0800

    t

--- windows/cmd-alias.bat
+++
@@ -1,8 +0,0 @@
-@echo "regedit -> HKEY_LOCAL_MACHINE\Software\Microsoft\Command Processor -> add String [AutoRun path] "
-doskey gtck=git checkout $*
-doskey gtmg=git merge $*
-doskey gtph=git push origin $*
-doskey gtpl=git pull origin $*
-doskey gtbr=git branch $*
-doskey gtcl=git clone $*
-doskey gtss=git status $*
